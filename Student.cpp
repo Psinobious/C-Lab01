a67965eb935433b9a920a01b1718b54dcb4a495d
Psinobious <daniel22.daj@gmail.com>
2018-09-21 09:07:11 -0400

    Add files via upload

---
+++ Student.cpp
@@ -0,0 +1,26 @@
+#include "Student.hh"
+
+using namespace std;
+
+Student::Student(){
+
+}
+string Student::getFirstName(){
+
+}
+string Student::getLastName(){
+
+}
+string Student::getStudentID(){
+
+}
+void Student::setFirstName(string firstName){
+
+}
+void Student::setLastName(string lastName){
+
+}
+void Student::setStudentID(string studentID){
+
+}
+
